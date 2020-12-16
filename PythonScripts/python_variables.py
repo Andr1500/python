3448199b11a9cf7ca98292718266f652a34e1bf7
Andr1500 <andrii.shykhov@gmail.com>
2020-12-16 22:37:39 +0100

    variables, string

---
+++ PythonScripts/python_variables.py
@@ -0,0 +1,10 @@
+f_name = "Andr"
+l_name = "Pupkin"
+age = 22
+
+full_name = (f_name + " " + l_name)
+
+print(full_name + " " + str(age))
+
+ # key sensitive is important in variables
+
---
+++ PythonScripts/strings.py
@@ -0,0 +1,21 @@
+string1 = "text"
+
+name = "vasia pupkin"
+
+print(name.title())  # function for big first letters
+
+print(name.upper()) # function for big all letters
+
+print(name.lower()) # all letters are low
+
+print("string 1\nstring 2")  # \n is for separate on another string (like enter)
+
+print("message:\n\tmessage1\n\tmessage2") # \t like tab
+
+a = "   ..  vitia vova ...    "
+
+print(a.strip())  # function is cutting spaces from left and right side
+
+b = a.strip()
+b = b.strip(".")  # cutting dots
+print(b)
